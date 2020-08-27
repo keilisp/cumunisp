4098007396d58bbd0edfc4bdeaa54f0d7352be2d
gideon <druksasha@ukr.net>
2020-08-27 23:17:57 +0300

    environment setup

--- parsing.c
+++ parsing.c
@@ -369,7 +369,7 @@ lval *lval_take(lval *v, int i) {
   return x;
 }
 
-lval *lval_eval_sexpr(lval *v);
+lval *lval_eval_sexpr(lenv* e, lval *v);
 
 lval *lval_eval(lenv *e, lval *v) {
   if (v->type == LVAL_SYM) {
@@ -444,27 +444,6 @@ lval *builtin_op(lenv *e, lval *a, char *op) {
   return x;
 }
 
-lval *builtin_add(lenv *e, lval *a) { return builtin_op(e, a, "+"); }
-lval *builtin_sub(lenv *e, lval *a) { return builtin_op(e, a, "-"); }
-lval *builtin_mul(lenv *e, lval *a) { return builtin_op(e, a, "*"); }
-lval *builtin_div(lenv *e, lval *a) { return builtin_op(e, a, "/"); }
-
-// Add builitins functions
-void lenv_add_builtin(lenv *e, char *name, lbuiltin func) {
-  lval *k = lval_sym(name);
-  lval *v = lval_fun(func);
-  lenv_put(e, k, v);
-  lval_del(k);
-  lval_del(v);
-}
-
-void lenv_add_builtins(lenv *e) {
-  // List Functions
-  lenv_add_builtin(e, "list", builtin_list);
-
-  // Mathematical Functions
-  lenv_add_builtin(e, "+", builtin_add);
-}
 
 #define LASSERT(args, cond, err)                                               \
   if (!(cond)) {                                                               \
@@ -472,7 +451,7 @@ void lenv_add_builtins(lenv *e) {
     return lval_err(err);                                                      \
   }
 
-lval *builtin_head(lval *a) {
+lval *builtin_head(lenv *e, lval *a) {
   // Check Error Conditions
   LASSERT(a, a->count == 1, "Function 'head' passed to many arguments!");
   LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
@@ -490,7 +469,7 @@ lval *builtin_head(lval *a) {
   return v;
 }
 
-lval *builtin_tail(lval *a) {
+lval *builtin_tail(lenv *e, lval *a) {
   // Check Error Conditions
   LASSERT(a, a->count == 1, "Function 'tail' passed to many arguments!");
   LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
@@ -506,14 +485,14 @@ lval *builtin_tail(lval *a) {
   return v;
 }
 
-lval *builtin_eval(lval *a) {
+lval *builtin_eval(lenv *e, lval *a) {
   LASSERT(a, a->count == 1, "Function 'eval' passed too many arguments!");
   LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
           "Function 'eval' passed incorrect type!");
 
   lval *x = lval_take(a, 0);
   x->type = LVAL_SEXPR;
-  return lval_eval(x);
+  return lval_eval(e, x);
 }
 
 lval *lval_join(lval *x, lval *y) {
@@ -526,7 +505,7 @@ lval *lval_join(lval *x, lval *y) {
   return x;
 }
 
-lval *builtin_join(lval *a) {
+lval *builtin_join(lenv *e, lval *a) {
   for (int i = 0; i < a->count; i++) {
     LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
             "Function 'join' passed incorrect type.")
@@ -541,18 +520,18 @@ lval *builtin_join(lval *a) {
   return x;
 }
 
-lval *builtin_list(lval *a) {
+lval *builtin_list(lenv *e ,lval *a) {
   a->type = LVAL_QEXPR;
   return a;
 }
 
-lval *builtin_cons(lval *a) {
+lval *builtin_cons(lenv *e, lval *a) {
   LASSERT(a, a->cell[1]->type == LVAL_QEXPR,
           "Function 'cons' passed incorrect type!");
   LASSERT(a, a->count == 2, "Function 'cons' passed too many arguments");
 
   // Create list of the entire expr
-  lval *x = builtin_list(a);
+  lval *x = builtin_list(e, a);
   // Pop the qexpr
   lval *y = lval_pop(a, 1);
   // Append cell[0] to the qexpr
@@ -561,7 +540,7 @@ lval *builtin_cons(lval *a) {
   return list;
 }
 
-lval *builtin_len(lval *a) {
+lval *builtin_len(lenv *e ,lval *a) {
   LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
           "Function 'len' passed incorrect type!");
   LASSERT(a, a->count == 1, "Function 'len' passed too many arguments");
@@ -569,7 +548,7 @@ lval *builtin_len(lval *a) {
   return lval_num(a->cell[0]->count);
 }
 
-lval *builtin_init(lval *a) {
+lval *builtin_init(lenv *e, lval *a) {
   // Check Error Conditions
   LASSERT(a, a->count == 1, "Function 'init' passed to many arguments!");
   LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
@@ -578,7 +557,7 @@ lval *builtin_init(lval *a) {
           "Function 'init' passed empty quote expression");
 
   // Get the length of qexpr
-  lval *x = builtin_len(a);
+  lval *x = builtin_len(e, a);
   int len = (int)x->num;
   // Take only last elem
   lval *v = lval_take(a->cell[0], len - 1);
@@ -586,57 +565,57 @@ lval *builtin_init(lval *a) {
   return v;
 }
 
-lval *builtin(lval *a, char *func) {
+lval *builtin(lenv *e, lval *a, char *func) {
   if (strcmp("list", func) == 0) {
-    return builtin_list(a);
+    return builtin_list(e, a);
   }
   if (strcmp("head", func) == 0) {
-    return builtin_head(a);
+    return builtin_head(e, a);
   }
   if (strcmp("tail", func) == 0) {
-    return builtin_tail(a);
+    return builtin_tail(e, a);
   }
   if (strcmp("join", func) == 0) {
-    return builtin_join(a);
+    return builtin_join(e, a);
   }
   if (strcmp("eval", func) == 0) {
-    return builtin_eval(a);
+    return builtin_eval(e, a);
   }
   if (strcmp("cons", func) == 0) {
-    return builtin_cons(a);
+    return builtin_cons(e, a);
   }
   if (strcmp("len", func) == 0) {
-    return builtin_len(a);
+    return builtin_len(e, a);
   }
   if (strcmp("init", func) == 0) {
-    return builtin_init(a);
+    return builtin_init(e, a);
   }
   if (strcmp("add", func) == 0) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
   if (strcmp("sub", func) == 0) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
   if (strcmp("mul", func) == 0) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
   if (strcmp("div", func) == 0) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
   if (strcmp("rem", func) == 0) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
   if (strcmp("pow", func) == 0) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
   if (strcmp("min", func) == 0) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
   if (strcmp("max", func) == 0) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
   if (strstr("+-*/%^", func)) {
-    return builtin_op(a, func);
+    return builtin_op(e, a, func);
   }
 
   lval_del(a);
@@ -680,6 +659,58 @@ lval *lval_eval_sexpr(lenv *e, lval *v) {
   return result;
 }
 
+lval *builtin_add(lenv *e, lval *a) { return builtin_op(e, a, "+"); }
+lval *builtin_sub(lenv *e, lval *a) { return builtin_op(e, a, "-"); }
+lval *builtin_mul(lenv *e, lval *a) { return builtin_op(e, a, "*"); }
+lval *builtin_div(lenv *e, lval *a) { return builtin_op(e, a, "/"); }
+lval *builtin_rem(lenv *e, lval *a) { return builtin_op(e, a, "rem"); }
+lval *builtin_pow(lenv *e, lval *a) { return builtin_op(e, a, "pow"); }
+lval *builtin_min(lenv *e, lval *a) { return builtin_op(e, a, "min"); }
+lval *builtin_max(lenv *e, lval *a) { return builtin_op(e, a, "max"); }
+/* lval *builtin_list(lenv *e, lval *a) { return bl_list(e, a); } */
+/* lval *builtin_head(lenv *e, lval *a) { return bl_head(e, a); } */
+/* lval *builtin_tail(lenv *e, lval *a) { return bl_tail(e, a); } */
+/* lval *builtin_eval(lenv *e, lval *a) { return bl_eval(e, a); } */
+/* lval *builtin_join(lenv *e, lval *a) { return bl_join(e, a); } */
+/* lval *builtin_cons(lenv *e, lval *a) { return bl_cons(e, a); } */
+/* lval *builtin_init(lenv *e, lval *a) { return bl_init(e, a); } */
+/* lval *builtin_len(lenv *e, lval *a) { return bl_len(e, a); } */
+
+// Add builitins functions
+void lenv_add_builtin(lenv *e, char *name, lbuiltin func) {
+  lval *k = lval_sym(name);
+  lval *v = lval_fun(func);
+  lenv_put(e, k, v);
+  lval_del(k);
+  lval_del(v);
+}
+
+void lenv_add_builtins(lenv *e) {
+  // List Functions
+  lenv_add_builtin(e, "list", builtin_list);
+  lenv_add_builtin(e, "head", builtin_head);
+  lenv_add_builtin(e, "tail", builtin_tail);
+  lenv_add_builtin(e, "eval", builtin_eval);
+  lenv_add_builtin(e, "join", builtin_join);
+  lenv_add_builtin(e, "cons", builtin_cons);
+  lenv_add_builtin(e, "init", builtin_init);
+  lenv_add_builtin(e, "len", builtin_len);
+
+  // Mathematical Functions
+  lenv_add_builtin(e, "+", builtin_add);
+  lenv_add_builtin(e, "add", builtin_add);
+  lenv_add_builtin(e, "-", builtin_sub);
+  lenv_add_builtin(e, "sub", builtin_sub);
+  lenv_add_builtin(e, "*", builtin_mul);
+  lenv_add_builtin(e, "mul", builtin_mul);
+  lenv_add_builtin(e, "/", builtin_div);
+  lenv_add_builtin(e, "div", builtin_div);
+  lenv_add_builtin(e, "rem", builtin_rem);
+  lenv_add_builtin(e, "pow", builtin_pow);
+  lenv_add_builtin(e, "min", builtin_min);
+  lenv_add_builtin(e, "max", builtin_max);
+}
+
 int main() {
   // Create some Parsers
   mpc_parser_t *Number = mpc_new("number");
@@ -711,6 +742,9 @@ int main() {
   puts("Cumunisp Version 0.0.0.0.1");
   puts("Press Ctrl+c to Exit\n");
 
+  lenv* e = lenv_new();
+  lenv_add_builtins(e);
+
   while (1) {
     // Output prompt
     char *input = readline("cumunisp> ");
@@ -721,7 +755,7 @@ int main() {
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Cumunisp, &r)) {
 
-      lval *result = lval_eval(lval_read(r.output));
+      lval *result = lval_eval(e, lval_read(r.output));
       lval_println(result);
       lval_del(result);
       mpc_ast_delete(r.output);
@@ -735,6 +769,7 @@ int main() {
     free(input);
   }
 
+  lenv_del(e);
   // Undefine and delete Parsers
   mpc_cleanup(4, Number, Symbol, Sexpr, Qexpr, Expr, Cumunisp);
   return 0;
